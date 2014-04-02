aba448b439154f056451963d7dcb0ff6b24cc1ff
Michał Taszycki <michal.taszycki@getbase.com>
2014-04-02 20:04:29 +0200

    Automated images slideshow

--- lib/offer_helpers.rb
+++ lib/offer_helpers.rb
@@ -1,6 +1,6 @@
 module OfferHelpers
   def workshop(options, &block)
     content = block ? capture_html(&block) : "BRABRA"
-    concat_content(partial("workshop", locals: { thumbnail: options[:thumbnail], image: options[:image], title: options[:title], content: content }))
+    concat_content(partial("workshop", locals: { thumbnail: options[:thumbnail], image: options[:image], title: options[:title], content: content, index: options[:index], images_count: options[:images_count] }))
   end
 end
